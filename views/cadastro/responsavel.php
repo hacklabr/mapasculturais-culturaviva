cb353fa092d70bcd74169b490fa40916bff3d577
Elvis Martins <elvisbmartins@gmail.com>
2015-09-22 13:56:09 -0300

    Estrutura pages Forms Cadastro

--- views/cadastro/responsavel.php
+++ views/cadastro/responsavel.php
@@ -3,3 +3,24 @@
 $this->layout = 'cadastro';
 
 $this->cadastroTitle = $this->dict('cadastro: titulo - responsavel', false);
+
+$this->cadastroText = 'Precisamos saber quem é você e pegar seus contatos! Afinal, comunicação é um requisito vital para que nossa rede se mantenha viva!';
+
+?>
+
+
+<section>
+    <h4>Informações Obrigatórias</h4>
+    <div class="form">
+        <!--  CAMPOS -->
+        
+    </div>
+</section>
+<section>
+    <h4>Informações Opcionais</h4>
+    <div class="form">
+        <!--  CAMPOS -->
+
+    </div>
+</section>
+
