4b6a3636323f09ffb790265e4a04787b01605e5c
Elvis Martins <elvisbmartins@gmail.com>
2015-09-22 13:03:59 -0300

    Criada estrutura da page FAQ

---
+++ views/rede/faq.php
@@ -0,0 +1,10 @@
+
+<section class="contanier">
+    <article>
+        <!-- img src="< ?php $this->asset('img/logo-rede-cultura-viva.png') ?>" / -->  
+        <p>Como faço para me autodeclarar um Ponto de Cultura? Insira sus dados no cadastro e apartir de uma análise simplificada a ser feita pelo Ministério da Cultura, a entidade ou coletivo passará fazer parte da Rede Cultura Viva, um ambiente de trocas de experiências, serviços, conhecimentos e informações. E se você já é ponto de Cultura entre na refe e faça parte do Circuito Cultura vida</p>
+        <a href="#" class="btn">Entrar na Rede</a>
+        <a href="#">Quais são os critérios para me autodeclarar um Ponto de Cultura?</a>
+    </article>  
+    <article></article>
+</section>
