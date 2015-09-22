c312101d7311b9cbf7785067f92d903ca2668103
Elvis Martins <elvisbmartins@gmail.com>
2015-09-22 13:58:36 -0300

    base sem css e images, pages redes

--- layouts/parts/footer.php
+++ layouts/parts/footer.php
@@ -1,6 +1,27 @@
 </section>
 <footer id="main-footer">
-    <?php echo __FILE__ ?>
+    <section>
+        <div class="colunm">
+            <h3>Informações úteis</h3>
+            <ul>
+                <li><a href="#" class="" >Sobre a Lei Cultura Viva</a></li>
+                <li><a href="#" class="" >Critérios da Autodeclaração</a></li>
+                <li><a href="#" class="" >Sobre a Rede Cultura Viva</a></li>
+                <li><a href="#" class="" >Privacidade e termos de Uso</a></li>
+                <li><a href="#" class="" >Repositório no Github</a></li>
+                <li><a href="#" class="" >Contato</a></li>
+            </ul>
+        </div>
+        <div class="colunm">
+            <a href="#"> <!-- img src="< ?php $this->asset('img/logo-rede-cultura-viva.png') ?>" / --> </a>
+            <a href="#"> <!-- img src="< ?php $this->asset('img/logo-rede-cultura-viva.png') ?>" / --> </a>
+            <a href="#"> <!-- img src="< ?php $this->asset('img/logo-rede-cultura-viva.png') ?>" / --> </a>
+        </div>
+    </section>
+    <section>
+        <a href="#"> <!-- img src="< ?php $this->asset('img/logo-rede-cultura-viva.png') ?>" / --> </a>
+        <a href="#"> <!-- img src="< ?php $this->asset('img/logo-rede-cultura-viva.png') ?>" / --> </a>
+    </section>
 </footer>
 <?php $this->part('templates'); ?>
 <?php //$this->bodyEnd(); ?>
