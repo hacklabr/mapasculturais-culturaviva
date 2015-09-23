066f81df08ceefc5017678978ae394673a33edad
Elvis Martins <elvisbmartins@gmail.com>
2015-09-23 16:53:02 -0300

    Estrutura e layout cadastro index e cadastro responsavel

--- layouts/cadastro.php
+++ layouts/cadastro.php
@@ -1,16 +1,30 @@
 <?php
 $this->part('header');
 ?>
-<header>
-    <h3><?php echo $this->cadastroTitle ?></h3>
-</header>
 
-<p><?php echo $this->cadastroText ?></p>
+<div id="page-<?php echo $this->cadastroPageClass ?>">
 
-<?php echo $TEMPLATE_CONTENT; ?>
+    <header>
+        <div class="icon <?php echo $this->cadastroIcon ?>"></div>
+        <h3><?php echo $this->cadastroTitle ?></h3>
+        
+        <div class="circle-status c100 p56 small">
+            <span>56%</span>
+            <div class="slice">
+                <div class="bar"></div>
+                <div class="fill"></div>
+            </div>
+       </div>
+    </header>
 
-<div> 
-        <a href="<?php echo $app->createUrl('cadastro', 'index'); ?>">voltar ao início</a>
+    <p><?php echo $this->cadastroText ?></p>
+
+    <?php echo $TEMPLATE_CONTENT; ?>
+
+    <div class="btn_voltar"> 
+       <a href="<?php echo $app->createUrl('cadastro', 'index'); ?>" >voltar ao início</a>
+    </div>
+    <div class="clear"></div>
 </div>
 
 <?php echo $this->part('footer'); ?>
--- views/cadastro/index.php
+++ views/cadastro/index.php
@@ -1,137 +1,188 @@
-<section>
-    <div class="messenger">
-        <p>Algumas informações já foram preenchidas de acordo com o cadastro que o MinC possui de seu Ponto. Confira com essas informações antes de validá-las!</p>
-    </div>
-    <article>
-        <h2>NOME DO PONTO<br />Página do seu ponto</h2>
-        <p>Esta é a página do seu ponto de cultura. Apenas você tem acesso a ela.</p>
-        <p>Fique a vontade apra ir preenchendo as sessões -  você não precisa fazer tudo agora! Quanto sua página estiver completa, clique em "validar dados" e aguarde de xx a xx dias para obter uma resposta da Rede Cultura Viva.</p>
-        <p>Depois, seu ponto poderá criar eventos, projetos e usar a plataforma para se manter em contato com o MinC, se inscrevendo em editais.</p>
-    </article>
-</section>
-<section class="boxs-cadastro">
-    <article class="box-info-responsavel">
-        <header>
-            <a href="<?php echo $app->createUrl('cadastro', 'responsavel'); ?>">
-                <span class="icon"></span>
-                <h4>Informações do Responsável</h4>
-                <span class="btn_mais"> + </span>
-            </a>
-        </header>
-        <div class="infos">
-            <p>Precisamos saber quem é você e pegar seus contatos!</p>
-            <span class="circle-status">50%</span>
+<div id="page-cadastro">
+    <section class="texto">
+        <div class="messenger">
+            <a href="#" class="close">X</a>
+            <p>Algumas informações já foram preenchidas de acordo com o cadastro que o MinC possui de seu Ponto. Confira com essas informações antes de validá-las!</p>
         </div>
-    </article>
-    <article class="box-ponto-mapa">
-        <header>
-            <a href="<?php echo $app->createUrl('cadastro', 'pontoMapa'); ?>">
-                <span class="icon"></span>
-                <h4>Seu Ponto no Mapa</h4>
-                <span class="btn_mais"> + </span>
-            </a>
-        </header>
-        <div class="infos">
-            <p>Precisamos saber quem é você e pegar seus contatos!</p>
-            <span class="circle-status">50%</span>
-        </div>
-    </article>
-    <article class="box-entidade-dados">
-        <header>
-            <a href="<?php echo $app->createUrl('cadastro', 'entidadeDados'); ?>">
-                <span class="icon"></span>
-                <h4>Dados da Entidade</h4>
-                <span class="btn_mais"> + </span>
-            </a>
-        </header>
-        <div class="infos">
-            <p>Precisamos saber quem é você e pegar seus contatos!</p>
-            <span class="circle-status">50%</span>
-        </div>
-    </article>
-    <article class="box-portfolio">
-        <header>
-            <a href="<?php echo $app->createUrl('cadastro', 'portifolio'); ?>">
-                <span class="icon"></span>
-                <h4>Portifólio</h4>
-                <span class="btn_mais"> + </span>
-            </a>
-        </header>
-        <div class="infos">
-            <p>Precisamos saber quem é você e pegar seus contatos!</p>
-            <span class="circle-status">50%</span>
-        </div>
-    </article>
-    <article class="box-entidade-contato">
-        <header>
-            <a href="<?php echo $app->createUrl('cadastro', 'entidadeContatos'); ?>">
-                <span class="icon"></span>
-                <h4>Contado da Entidade</h4>
-                <span class="btn_mais"> + </span>
-            </a>
-        </header>
-        <div class="infos">
-            <p>Precisamos saber quem é você e pegar seus contatos!</p>
-            <span class="circle-status status-51">50%</span>
-        </div>
-    </article>
-    <article class="box-fale-mais-ponto">
-        <header>
-            <a href="<?php echo $app->createUrl('cadastro', 'pontoMais'); ?>">
-                <span class="icon"></span>
-                <h4>Fale mais sobre seu Ponto</h4>
-                <span class="btn_mais"> + </span>
-            </a>
-        </header>
-        <div class="infos">
-            <p>Precisamos saber quem é você e pegar seus contatos!</p>
-            <span class="circle-status">50%</span>
-        </div>
-    </article>
-</section>
-<section class="box-status">
-    <article class="validar-ponto">
-        <p>validar</p>
-        <p>Para validar seu ponto, você precisa preencher todas as informações obrigatórias:</p>
-    </article>
-    <article>
-        <div class="status">
-            <span class="circle-status">50%</span>
-            <span class="icon">icon</span>
-            <p>Informações do responável<br />(50% informações opcionais)</p>
-        </div>
-        <div class="status">
-            <span class="circle-status">50%</span>
-            <span class="icon">icon</span>
-            <p>Informações do responável<br />(50% informações opcionais)</p>
-        </div>
-        <div class="status">
-            <span class="circle-status">50%</span>
-            <span class="icon">icon</span>
-            <p>Informações do responável<br />(50% informações opcionais)</p>
-        </div>
-        <div class="status">
-            <span class="circle-status">50%</span>
-            <span class="icon">icon</span>
-            <p>Informações do responável<br />(50% informações opcionais)</p>
-        </div>
-        <div class="status">
-            <span class="circle-status">50%</span>
-            <span class="icon">icon</span>
-            <p>Informações do responável<br />(50% informações opcionais)</p>
-        </div>
-        <div class="status">
-            <span class="circle-status">50%</span>
-            <span class="icon">icon</span>
-            <p>Informações do responável<br />(50% informações opcionais)</p>
-        </div>
-        <div class="clear"></div>
-        <div class="infos-messenge">
-            Algumas informações já foram preenchidas de acordo com o cadastro que o MinC possui de seu Ponto. Configra com atenção essas informações antes de validá-las!
-        </div>
-        <div class="btn-validar">
-            <button>Validar</button>
-        </div>
-    </article>
-</section>
+        <article>
+            <h2>NOME DO PONTO<br /><span>Página do seu ponto</span></h2>
+            <p>Esta é a página do seu ponto de cultura. Apenas você tem acesso a ela.</p>
+            <p>Fique a vontade apra ir preenchendo as sessões -  você não precisa fazer tudo agora! Quanto sua página estiver completa, clique em "validar dados" e aguarde de xx a xx dias para obter uma resposta da Rede Cultura Viva.</p>
+            <p>Depois, seu ponto poderá criar eventos, projetos e usar a plataforma para se manter em contato com o MinC, se inscrevendo em editais.</p>
+        </article>
+    </section>
+    <section class="boxs-cadastro">
+        <article class="box-info-responsavel">
+            <header>
+                <a href="<?php echo $app->createUrl('cadastro', 'responsavel'); ?>">
+                    <span class="icon icon_profile"></span>
+                    <h4>Informações do Responsável</h4>
+                    <span class="btn_mais"> + </span>
+                </a>
+            </header>
+            <div class="infos">
+                <div class="texto">
+                     <p>Precisamos saber quem é você e pegar seus contatos!</p>
+                </div>
+                <div class="circle-status c100 p56 small">
+                    <span>56%</span>
+                    <div class="slice">
+                        <div class="bar"></div>
+                        <div class="fill"></div>
+                    </div>
+                </div>
+            </div>
+        </article>
+        <article class="box-ponto-mapa">
+            <header>
+                <a href="<?php echo $app->createUrl('cadastro', 'pontoMapa'); ?>">
+                    <span class="icon icon_profile"></span>
+                    <h4>Seu Ponto no Mapa</h4>
+                    <span class="btn_mais"> + </span>
+                </a>
+            </header>
+            <div class="infos">
+                <div class="texto">
+                     <p>Precisamos saber quem é você e pegar seus contatos!</p>
+                </div>
+                <div class="circle-status c100 p56">
+                    <span>56%</span>
+                    <div class="slice">
+                        <div class="bar"></div>
+                        <div class="fill"></div>
+                    </div>
+                </div>
+            </div>
+        </article>
+        <article class="box-entidade-dados">
+            <header>
+                <a href="<?php echo $app->createUrl('cadastro', 'entidadeDados'); ?>">
+                    <span class="icon"></span>
+                    <h4>Dados da Entidade</h4>
+                    <span class="btn_mais"> + </span>
+                </a>
+            </header>
+            <div class="infos">
+                <div class="texto">
+                     <p>Precisamos saber quem é você e pegar seus contatos!</p>
+                </div>
+                <div class="circle-status c100 p56">
+                    <span>56%</span>
+                    <div class="slice">
+                        <div class="bar"></div>
+                        <div class="fill"></div>
+                    </div>
+                </div>
+            </div>
+        </article>
+        <article class="box-portfolio">
+            <header>
+                <a href="<?php echo $app->createUrl('cadastro', 'portifolio'); ?>">
+                    <span class="icon"></span>
+                    <h4>Portifólio</h4>
+                    <span class="btn_mais"> + </span>
+                </a>
+            </header>
+            <div class="infos">
+                <div class="texto">
+                     <p>Precisamos saber quem é você e pegar seus contatos!</p>
+                </div>
+                <div class="circle-status c100 p56">
+                    <span>56%</span>
+                    <div class="slice">
+                        <div class="bar"></div>
+                        <div class="fill"></div>
+                    </div>
+                </div>
+            </div>
+        </article>
+        <article class="box-entidade-contato">
+            <header>
+                <a href="<?php echo $app->createUrl('cadastro', 'entidadeContatos'); ?>">
+                    <span class="icon"></span>
+                    <h4>Contado da Entidade</h4>
+                    <span class="btn_mais"> + </span>
+                </a>
+            </header>
+            <div class="infos">
+               <div class="texto">
+                     <p>Precisamos saber quem é você e pegar seus contatos!</p>
+                </div>
+                <div class="circle-status c100 p56">
+                    <span>56%</span>
+                    <div class="slice">
+                        <div class="bar"></div>
+                        <div class="fill"></div>
+                    </div>
+                </div>
+            </div>
+        </article>
+        <article class="box-fale-mais-ponto">
+            <header>
+                <a href="<?php echo $app->createUrl('cadastro', 'pontoMais'); ?>">
+                    <span class="icon"></span>
+                    <h4>Fale mais sobre seu Ponto</h4>
+                    <span class="btn_mais"> + </span>
+                </a>
+            </header>
+            <div class="infos">
+               <div class="texto">
+                     <p>Precisamos saber quem é você e pegar seus contatos!</p>
+                </div>
+                <div class="circle-status c100 p56">
+                    <span>56%</span>
+                    <div class="slice">
+                        <div class="bar"></div>
+                        <div class="fill"></div>
+                    </div>
+                </div>
+            </div>
+        </article>
+    </section>
+    <section class="box-status">
+        <article class="validar-ponto">
+            <p>validar</p>
+            <p>Para validar seu ponto, você precisa preencher todas as informações obrigatórias:</p>
+        </article>
+        <article>
+            <div class="status">
+                <span class="circle-status">50%</span>
+                <span class="icon">icon</span>
+                <p>Informações do responável<br />(50% informações opcionais)</p>
+            </div>
+            <div class="status">
+                <span class="circle-status">50%</span>
+                <span class="icon">icon</span>
+                <p>Informações do responável<br />(50% informações opcionais)</p>
+            </div>
+            <div class="status">
+                <span class="circle-status">50%</span>
+                <span class="icon">icon</span>
+                <p>Informações do responável<br />(50% informações opcionais)</p>
+            </div>
+            <div class="status">
+                <span class="circle-status">50%</span>
+                <span class="icon">icon</span>
+                <p>Informações do responável<br />(50% informações opcionais)</p>
+            </div>
+            <div class="status">
+                <span class="circle-status">50%</span>
+                <span class="icon">icon</span>
+                <p>Informações do responável<br />(50% informações opcionais)</p>
+            </div>
+            <div class="status">
+                <span class="circle-status">50%</span>
+                <span class="icon">icon</span>
+                <p>Informações do responável<br />(50% informações opcionais)</p>
+            </div>
+            <div class="clear"></div>
+            <div class="infos-messenge">
+                Algumas informações já foram preenchidas de acordo com o cadastro que o MinC possui de seu Ponto. Configra com atenção essas informações antes de validá-las!
+            </div>
+            <div class="btn-validar">
+                <button>Validar</button>
+            </div>
+        </article>
+    </section>
+</div>
 
