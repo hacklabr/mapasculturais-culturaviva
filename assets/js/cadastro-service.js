06ce5daac2af1e51d5e3a7109711cd14e38b49fb
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2015-09-24 17:31:42 -0300

    ajusta controllers, campos

--- assets/js/cadastro-service.js
+++ assets/js/cadastro-service.js
@@ -11,43 +11,28 @@
     });
 
 
-    app.factory('Responsible', ['$resource', '$http', 'MapasCulturais',
+    app.factory('Agent', ['$resource', '$http', 'MapasCulturais',
         function($resource, $http, MapasCulturais){
             var resourceConfig = {
                 'get':  {
                     'method':'GET',
                     'params':{
-                        '@select': 'id,singleUrl,name,'+
-                            'rg,rg_orgao,relacaoPonto,cpf,'+
-                            'geoEstado,emailPrivado,telefone1,'+
-                            'telefone1_operadora,nomeCompleto,'+
-                            'geoCidade,facebook,twitter,googleplus',
+                        '@select': 'id,singleUrl,name,rg,rg_orgao,relacaoPonto,cpf,geoEstado,'+
+                                   'emailPrivado,telefone1,telefone1_operadora,nomeCompleto,'+
+                                   'geoCidade,facebook,twitter,googleplus',
                         '@permissions': 'view'
                     }
-                },
-                'patch': {
-                    'method': 'PATCH',
                 }
             };
 
             var getUrl = '/api/agent/findOne?id=EQ(:id)';
-            var Responsible = $resource(getUrl, {'id': '@id'}, resourceConfig);
+            var Agent = $resource(getUrl, {'id': '@id'}, resourceConfig);
 
-            // 1) o angular não deixa selecionar individualmente o campos para patch
-            // 2) a api do mapas não devolve o objeto (nem parte dele) após a consulta
-            Responsible.prototype.patch = function patch_responsible(field) {
-                if(!this.id) {
-                    throw new Error('Não é possível salvar sem o id');
-                }
-
-                if(!this.hasOwnProperty(field)){
-                    throw new Error('Não foi informado o campo que se quer salvar');
-                }
-
-                // TODO: @rafa - Qual o endereço para patch?
+            Agent.prototype.patch = function patch_responsible(field) {
                 var patchUrl = MapasCulturais.createUrl('agent','single',[this.id]);
-                if(!patchUrl) {
-                    throw new Error('O agente não tem o endereço para PATCH');
+
+                if(!(this.id && this.hasOwnProperty(field) && patchUrl)) {
+                    throw new Error('REQUIRED: id:' + this.id + '; field:' + field + '; url:' + patchUrl);
                 }
 
                 var data = {};
@@ -60,7 +45,7 @@
                 });
             };
 
-            return Responsible;
+            return Agent;
         }
     ]);
 
