0feef11f62649bb1fe5029773d16d4ea4e8844c5
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2015-09-24 09:53:13 -0300

    evita que valores undefined null false sejam salvos

--- assets/js/cadastro-controller.js
+++ assets/js/cadastro-controller.js
@@ -13,8 +13,8 @@
             var _saved_agent = angular.copy($scope.agent);
 
             $scope.save_field = function save_field(field) {
-                var new_value = $scope.agent[field];
-                var old_value = _saved_agent[field];
+                var new_value = $scope.agent[field] || "";
+                var old_value = _saved_agent[field] || "";
 
                 if((new_value || old_value) && new_value !== old_value) {
                     $scope.agent.patch(field).then(function(){
