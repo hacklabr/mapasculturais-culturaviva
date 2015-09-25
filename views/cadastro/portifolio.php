aa55e6d186dbdd312d890f462eaedb039d0ea6a1
Elvis Martins <elvisbmartins@gmail.com>
2015-09-25 12:39:50 -0300

    Page Porftfolio

--- views/cadastro/portifolio.php
+++ views/cadastro/portifolio.php
@@ -20,8 +20,9 @@
             <label class="colunm-50">
                 
                 <p>Caso não possua portifólio online, você também pode anexar arquivos no formato pdf, com no máximo 20kb.</p>
-                <span class="destaque"><i>?</i></span>
-                <p>Precisa de ajuda para montar seu portifólio?
+                
+                <p><span class="destaque"><i>?</i></span>
+                    Precisa de ajuda para montar seu portifólio?
                     <br>
                     <a href="#">Clique aqui</a> para baixar um modelo com orientações.
                 </p>
@@ -29,133 +30,71 @@
         </div>  
         <div class="clear"></div>
         <div class="row">
-            <label class="colunm1">
-                <span class="destaque">Qual sua relação com o Ponto/Pontão de Cultura?* <i>?</i></span>
-                <input type="text" ng-blur="save_field('relacao_ponto')" ng-model="agent.relacao_ponto"/>
-            </label>
-
-            <label class="colunm2">
-                <span>CPF*</span>
-                <input type="text" ng-blur="save_field('cpf')" ng-model="agent.cpf" />
-            </label>
-
-            <label class="colunm3">
-                <span>Estado*</span>
-                <select ng-blur="save_field('geoEstado')" ng-model="agent.geoEstado">
-                    <option value="AC">Acre</option>
-                    <option value="AL">Alagoas</option>
-                    <option value="AP">Amapá</option>
-                    <option value="AM">Amazonas</option>
-                    <option value="BA">Bahia</option>
-                    <option value="CE">Ceará</option>
-                    <option value="DF">Distrito Federal</option>
-                    <option value="ES">Espírito Santo</option>
-                    <option value="GO">Goiás</option>
-                    <option value="MA">Maranhão</option>
-                    <option value="MT">Mato Grosso</option>
-                    <option value="MS">Mato Grosso do Sul</option>
-                    <option value="MG">Minas Gerais</option>
-                    <option value="PA">Pará</option>
-                    <option value="PB">Paraíba</option>
-                    <option value="PR">Paraná</option>
-                    <option value="PE">Pernambuco</option>
-                    <option value="PI">Piauí</option>
-                    <option value="RJ">Rio de Janeiro</option>
-                    <option value="RN">Rio Grande do Norte</option>
-                    <option value="RS">Rio Grande do Sul</option>
-                    <option value="RO">Rondônia</option>
-                    <option value="RR">Roraima</option>
-                    <option value="SC">Santa Catarina</option>
-                    <option value="SP">São Paulo</option>
-                    <option value="SE">Sergipe</option>
-                    <option value="TO">Tocantins</option>
-                </select>
-            </label>
-        </div>
-        <div class="clear"></div>
-        <div class="row">
-            <label class="colunm1">
-                <span>E-mail Pessoal*</span>
-                <input type="email" ng-blur="save_field('emailPrivado')" ng-model="agent.emailPrivado" />
-            </label>
-
-            <label class="colunm2">
-                <span>Telefone Pessoal (com DDD)*</span>
-                <input type="text" ng-blur="save_field('telefone1')" ng-model="agent.telefone1"/>
-            </label>
-
-            <label class="colunm3">
-                <span>Operadora*</span>
-                <select ng-blur="save_field('telefone1_operadora')" ng-model="agent.telefone1_operadora">
-                    <option>51 Brasil</option>                <option>Intelig</option>
-                    <option>Aerotech</option>                 <option>ITACEU</option>
-                    <option>Alpamayo</option>                 <option>Konecta</option>
-                    <option>Alpha Nobilis*</option>           <option>LigueMAX</option>
-                    <option>America Net</option>              <option>LinkNET</option>
-                    <option>Amigo</option>                    <option>Locaweb</option>
-                    <option>BBT Brasil</option>               <option>Nebracam</option>
-                    <option>Cabo Telecom</option>             <option>Neotelecom</option>
-                    <option>Cambridge</option>                <option>Nextel</option>
-                    <option>Convergia</option>                <option>Nexus</option>
-                    <option>CTBC</option>                     <option>Oi</option>
-                    <option>DIALDATA TELECOM</option>         <option>Ostara</option>
-                    <option>Dollarphone</option>              <option>OTS</option>
-                    <option>DSLI</option>                     <option>Plenna</option>
-                    <option>Easytone</option>                 <option>Redevox</option>
-                    <option>Embratel / NET / Claro</option>   <option>Sercomtel</option>
-                    <option>Engevox</option>                  <option>Sermatel</option>
-                    <option>Epsilon</option>                  <option>SmartTelecom|76Telecom</option>
-                    <option>Espas</option>                    <option>Spin</option>
-                    <option>Fale 65</option>                  <option>Telebit</option>
-                    <option>Falkland/IPCorp</option>          <option>Teledados</option>
-                    <option>Fonar</option>                    <option>TIM</option>
-                    <option>Global Crossing(Impsat)</option>  <option>Transit Telecom</option>
-                    <option>Golden Line</option>              <option>Viacom</option>
-                    <option>GT Group</option>                 <option>Viper</option>
-                    <option>GVT</option>                      <option>Vipway</option>
-                    <option>Hello Brazil</option>             <option>Vivo</option>
-                    <option>Hit Telecomunicações</option>     <option>Voitel</option>
-                    <option>Hoje</option>
-                    <option>IDT (Minas Gerais)</option>
-                </select>
-            </label>
+            <label class="colunm-full">
+                <span class="destaque">Atividades culturais em realização* <i>?</i></span>
+                <textarea></textarea>
+            </label>          
         </div>
         <div class="clear"></div>
     </div>
     <div class="form form-opcional">
         <h4>Informações Opcionais</h4>
-        <div class="img_updade">
-            <img src="<?php $this->asset('img/incluir_img.png') ?>">
+        <div class="row">
+            <span class="destaque destaque-conecte">Conecte seu ponto com as redes sociais: <i>?</i></span>
         </div>
-        <label class="upadete_foto">
-            <span>Incluir foto</span>
-            <input type="file" ng-blur="save_field('photo')" ng-model="agent.photo"/>
-        </label>
+        <div class="row">
+            <label class="colunm-redes site-oficial">
+                <span><i class="icon icon-location"></i> Site oficial</span>
+                <input type="text" ng-blur="save_field('facebook')" ng-model="agent.facebook" placeholder="http://"/>
+            </label>
+        </div>
+        <div class="row">
+            <label class="colunm-redes facebook">
+                <span><i class="icon icon-facebook-squared"></i> Página Facebook</span>
+                <input type="text" ng-blur="save_field('facebook')" ng-model="agent.facebook" placeholder="http://"/>
+            </label>
 
-        <label class="nome_chamado">
-            <span class="destaque">Qual nome você gostaria de ser chamado <i>?</i></span>
-            <input type="text" ng-blur="save_field('name')" ng-model="agent.name"/>
-        </label>
+            <label class="colunm-redes twitter">
+                <span><i class="icon icon-twitter"></i> Perfil no Twitter</span>
+                <input type="text" ng-blur="save_field('twitter')" ng-model="agent.twitter" placeholder="http://"/>
+            </label>
 
-        <label class="cidade">
-            <span>Cidade</span>
-            <input type="text" ng-blur="save_field('city')" ng-model="agent.city"/>
-        </label>
+            <label class="colunm-redes googleplus">
+                <span><i class="icon icon-gplus"></i> Perfil no Google+</span>
+                <input type="text" ng-blur="save_field('googleplus')" ng-model="agent.googleplus" placeholder="http://"/>
+            </label>
+        </div>
+        <div class="row">
+            <label class="colunm-redes flick">
+                <span><img src="<?php $this->asset('img/icon_flicker.png') ?>"> Página no Flickr</span>
+                <input type="text" ng-blur="save_field('facebook')" ng-model="agent.facebook" placeholder="http://"/>
+            </label>
 
-        <span class="destaque redessociais">Seu perfil nas redes sociais: <i>?</i></span>
-        <label class="facebook">
-            <span>Seu perfil no Facebook</span>
-            <input type="text" ng-blur="save_field('facebook')" ng-model="agent.facebook" placeholder="http://"/>
-        </label>
+            <label class="colunm-redes diaspora">
+                <span><img src="<?php $this->asset('img/icon_diaspora.png') ?>"> Perfil na Diáspora:</span>
+                <input type="text" ng-blur="save_field('twitter')" ng-model="agent.twitter" placeholder="http://"/>
+            </label>
 
-        <label class="twitter">
-            <span>Seu perfil no Twitter</span>
-            <input type="text" ng-blur="save_field('twitter')" ng-model="agent.twitter" placeholder="http://"/>
-        </label>
+            <label class="colunm-redes youtube">
+                <span><img src="<?php $this->asset('img/icon_youtube.png') ?>"> Perfil no Youtube:</span>
+                <input type="text" ng-blur="save_field('googleplus')" ng-model="agent.googleplus" placeholder="http://"/>
+            </label>
+        </div>
+        <div class="clear"></div>
+        <div class="row">
+            <label class="colunm-full">
+                <span class="destaque">Conte um pouco(800 caractres) sobre a história do ponto de Cultura <i>?</i></span>
+                <textarea></textarea>
+            </label>          
+        </div>
+        <div class="clear"></div>
+        <div class="row">
+            <label class="colunm-full">
+                <span class="destaque">Fotos de Divulgação do Ponto de Cultura <i>?</i></span>
+                <img src="<?php $this->asset('img/incluir_img.png') ?>" />
+                <p>Inclua no máximo x arquivos, no formato JPG ou PNG com até xxKB</p>
+            </label>
 
-        <label class="googleplus">
-            <span>Seu perfil no Google+</span>
-            <input type="text" ng-blur="save_field('googleplus')" ng-model="agent.googleplus" placeholder="http://"/>
-        </label>
+        </div>  
     </div>
 </form>
