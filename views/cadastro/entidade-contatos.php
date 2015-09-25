aa1776828b151e676081fc05537175d9f70912cf
Elvis Martins <elvisbmartins@gmail.com>
2015-09-25 13:20:40 -0300

    Page Contatos de Entidade

--- views/cadastro/entidade-contatos.php
+++ views/cadastro/entidade-contatos.php
@@ -12,38 +12,80 @@
     <div class="form">
         <h4>Informações Obrigatórias</h4>
         <div class="row">
-            <label class="colunm1">
-                <span>Nome completo*</span>
-                <input type="text" ng-blur="save_field('nomeCompleto')" ng-model="agent.nomeCompleto" />
-            </label>
-
-            <label class="colunm2">
-                <span>RG*</span>
-                <input type="text" ng-blur="save_field('rg')" ng-model="agent.rg"/>
-            </label>
-
-            <label class="colunm3">
-                <span>Órgão expeditor*</span>
-                <select ng-blur="save_field('rg_orgao')" ng-model="agent.rg_orgao">
-                    <option value="SSP">Secretaria de Segurança Pública</option>
-                </select>
+            <label class="colunm-full">
+                <span>Email institucional*</span>
+                <input type="text" ng-blur="save_field('')" ng-model="email" />
             </label>
         </div>  
         <div class="clear"></div>
         <div class="row">
+            <label class="colunm05">
+                <span>Telefone institucional(com DD)*</span>
+                <input type="text" ng-blur="save_field('')" ng-model="phone"/>
+            </label>
+
+            <label class="colunm02">
+                <span>Operadora*</span>
+                <select ng-blur="save_field('')" ng-model="">
+                    <option value="AC">Acre</option>
+                    <option value="AL">Alagoas</option>
+                    <option value="AP">Amapá</option>
+                    <option value="AM">Amazonas</option>
+                </select>
+            </label>
+        </div>
+        <div class="clear"></div>
+        <div class="row">
+            <label class="colunm05">
+                <span>Outro Telefone(com DD)*</span>
+                <input type="text" ng-blur="save_field('')" ng-model="phone"/>
+            </label>
+
+            <label class="colunm02">
+                <span>Operadora*</span>
+                <select ng-blur="save_field('')" ng-model="">
+                    <option value="AC">Acre</option>
+                    <option value="AL">Alagoas</option>
+                    <option value="AP">Amapá</option>
+                    <option value="AM">Amazonas</option>
+                </select>
+            </label>
+        </div>
+        <div class="clear"></div>
+        <div class="row">
             <label class="colunm1">
-                <span class="destaque">Qual sua relação com o Ponto/Pontão de Cultura?* <i>?</i></span>
-                <input type="text" ng-blur="save_field('relacao_ponto')" ng-model="agent.relacao_ponto"/>
+                <span class="destaque">Nome do Responsável* <i>?</i></span>
+                <input type="text" ng-blur="save_field('name')" ng-model="name" />
             </label>
 
             <label class="colunm2">
-                <span>CPF*</span>
-                <input type="text" ng-blur="save_field('cpf')" ng-model="agent.cpf" />
+                <span>Cargo do Responsável*</span>
+                <input type="text" ng-blur="save_field('')" ng-model=""/>
+            </label>
+        </div>
+        <div class="clear"></div>
+        <div class="row">
+            <label class="colunm1">
+                <span>Email do Responsável* </span>
+                <input type="email" ng-blur="save_field('')" ng-model="email" />
             </label>
 
-            <label class="colunm3">
+            <label class="colunm2">
+                <span>Telefone do Responsável*</span>
+                <input type="text" ng-blur="save_field('')" ng-model=""/>
+            </label>
+        </div>
+        <div class="clear"></div>
+        <div class="row">
+            <label class="colunm1">
+                <span class="destaque">Endereço* <i>?</i></span>
+            </label>
+        </div>
+        <div class="clear"></div>
+        <div class="row">
+            <label class="colunm05">
                 <span>Estado*</span>
-                <select ng-blur="save_field('geoEstado')" ng-model="agent.geoEstado">
+                <select ng-blur="save_field('')" ng-model="">
                     <option value="AC">Acre</option>
                     <option value="AL">Alagoas</option>
                     <option value="AP">Amapá</option>
@@ -73,91 +115,31 @@
                     <option value="TO">Tocantins</option>
                 </select>
             </label>
+            <label class="colunm2">
+                <span>Cidade*</span>
+                <input type="text" ng-blur="save_field('')" ng-model=""/>
+            </label>
+            <label class="colunm3">
+                <span>Bairro*</span>
+                <input type="text" ng-blur="save_field('')" ng-model=""/>
+            </label>
         </div>
         <div class="clear"></div>
         <div class="row">
-            <label class="colunm1">
-                <span>E-mail Pessoal*</span>
-                <input type="email" ng-blur="save_field('emailPrivado')" ng-model="agent.emailPrivado" />
+            <label class="colunm05">
+                <span>Número* </span>
+                <input type="text" ng-blur="save_field('')" ng-model=""/>
             </label>
 
             <label class="colunm2">
-                <span>Telefone Pessoal (com DDD)*</span>
-                <input type="text" ng-blur="save_field('telefone1')" ng-model="agent.telefone1"/>
+                <span>Rua*</span>
+                <input type="text" ng-blur="save_field('')" ng-model=""/>
             </label>
-
             <label class="colunm3">
-                <span>Operadora*</span>
-                <select ng-blur="save_field('telefone1_operadora')" ng-model="agent.telefone1_operadora">
-                    <option>51 Brasil</option>                <option>Intelig</option>
-                    <option>Aerotech</option>                 <option>ITACEU</option>
-                    <option>Alpamayo</option>                 <option>Konecta</option>
-                    <option>Alpha Nobilis*</option>           <option>LigueMAX</option>
-                    <option>America Net</option>              <option>LinkNET</option>
-                    <option>Amigo</option>                    <option>Locaweb</option>
-                    <option>BBT Brasil</option>               <option>Nebracam</option>
-                    <option>Cabo Telecom</option>             <option>Neotelecom</option>
-                    <option>Cambridge</option>                <option>Nextel</option>
-                    <option>Convergia</option>                <option>Nexus</option>
-                    <option>CTBC</option>                     <option>Oi</option>
-                    <option>DIALDATA TELECOM</option>         <option>Ostara</option>
-                    <option>Dollarphone</option>              <option>OTS</option>
-                    <option>DSLI</option>                     <option>Plenna</option>
-                    <option>Easytone</option>                 <option>Redevox</option>
-                    <option>Embratel / NET / Claro</option>   <option>Sercomtel</option>
-                    <option>Engevox</option>                  <option>Sermatel</option>
-                    <option>Epsilon</option>                  <option>SmartTelecom|76Telecom</option>
-                    <option>Espas</option>                    <option>Spin</option>
-                    <option>Fale 65</option>                  <option>Telebit</option>
-                    <option>Falkland/IPCorp</option>          <option>Teledados</option>
-                    <option>Fonar</option>                    <option>TIM</option>
-                    <option>Global Crossing(Impsat)</option>  <option>Transit Telecom</option>
-                    <option>Golden Line</option>              <option>Viacom</option>
-                    <option>GT Group</option>                 <option>Viper</option>
-                    <option>GVT</option>                      <option>Vipway</option>
-                    <option>Hello Brazil</option>             <option>Vivo</option>
-                    <option>Hit Telecomunicações</option>     <option>Voitel</option>
-                    <option>Hoje</option>
-                    <option>IDT (Minas Gerais)</option>
-                </select>
+                <span>Complemento*</span>
+                <input type="text" ng-blur="save_field('')" ng-model=""/>
             </label>
         </div>
         <div class="clear"></div>
-    </div>
-    <div class="form form-opcional">
-        <h4>Informações Opcionais</h4>
-        <div class="img_updade">
-            <img src="<?php $this->asset('img/incluir_img.png') ?>">
-        </div>
-        <label class="upadete_foto">
-            <span>Incluir foto</span>
-            <input type="file" ng-blur="save_field('photo')" ng-model="agent.photo"/>
-        </label>
-
-        <label class="nome_chamado">
-            <span class="destaque">Qual nome você gostaria de ser chamado <i>?</i></span>
-            <input type="text" ng-blur="save_field('name')" ng-model="agent.name"/>
-        </label>
-
-        <label class="cidade">
-            <span>Cidade</span>
-            <input type="text" ng-blur="save_field('city')" ng-model="agent.city"/>
-        </label>
-
-        <span class="destaque redessociais">Seu perfil nas redes sociais: <i>?</i></span>
-        <label class="facebook">
-            <span>Seu perfil no Facebook</span>
-            <input type="text" ng-blur="save_field('facebook')" ng-model="agent.facebook" placeholder="http://"/>
-        </label>
-
-        <label class="twitter">
-            <span>Seu perfil no Twitter</span>
-            <input type="text" ng-blur="save_field('twitter')" ng-model="agent.twitter" placeholder="http://"/>
-        </label>
-
-        <label class="googleplus">
-            <span>Seu perfil no Google+</span>
-            <input type="text" ng-blur="save_field('googleplus')" ng-model="agent.googleplus" placeholder="http://"/>
-        </label>
     </div>
 </form>
