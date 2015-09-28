18246e4eef30a4ce54e3322a1ce0ac35ad267953
Elvis Martins <elvisbmartins@gmail.com>
2015-09-28 17:01:03 -0300

    Estruturacao de Class e ID

--- views/rede/index.php
+++ views/rede/index.php
@@ -1,4 +1,4 @@
-<section class="contanier">
+<section id="page-rede" >
     <article class="slide-home">
             <img src="<?php $this->asset('img/logo-rede-cultural-viva.png'); ?>" />
             <p>Como faço para me autodeclarar um Ponto de Cultura? Insira sus dados no cadastro e apartir de uma análise simplificada a ser feita pelo Ministério da Cultura, a entidade ou coletivo passará fazer parte da Rede Cultura Viva, um ambiente de trocas de experiências, serviços, conhecimentos e informações. E se você já é ponto de Cultura entre na refe e faça parte do Circuito Cultura vida</p>
@@ -12,7 +12,7 @@
         <div class="row">
             <h1>Somos todos<br />Pontos de Cultura</h1>
             <p>Desde 23 de julho de 2014, com a sanção da Lei Cultura Vida (Lei 13.018/2014), o Programa Cultura Viva e os Pontos de Cultura tornaram-se Política de Estado. Além de garantir a continuidade do Programa, a Lei simplifica e desburocratiza os processos de prestação de contas e o repasse de recursos para as organizações da sociedade civil. Uma grande vitória para o moviemnto cultural brasileiro!</p>
-            <a href="#" class="btn btn__active">Leia Mais</a>
+            <a href="<?php echo $app->createUrl('rede', 'faq'); ?>" class="btn btn__active">Leia Mais</a>
         </div>
         <div class="row">
             <div class="colunm">
