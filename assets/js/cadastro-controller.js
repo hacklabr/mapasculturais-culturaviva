c1e4fdc4219aa10f653abb3a84256a3184af9940
Bruno Martin <bruno.martin@hacklab.com.br>
2015-10-02 17:50:54 -0300

    remove alert de debug

--- assets/js/cadastro-controller.js
+++ assets/js/cadastro-controller.js
@@ -381,7 +381,6 @@
                 entity_update[field] = $scope.entity[field];
                 Entity.patch({'id': agent_id}, entity_update, function(entity){
                     $scope.msgs[field] = 'saved';
-                    alert('salvou!!!');
                 });
             };
         }
