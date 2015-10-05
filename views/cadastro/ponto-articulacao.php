45d0b0c60e6ff63e642d3fa361953bbad11ed08d
Diego Aguilera <dorgam@gmail.com>
2015-10-05 01:53:46 -0300

    correcoes articulaçao

--- views/cadastro/ponto-articulacao.php
+++ views/cadastro/ponto-articulacao.php
@@ -42,12 +42,12 @@
                 <span class="destaque title">Áreas de atuação</span>
             </div>
             <div class="row">
-                <span class="destaque">Especifique a área de experiência e temas que você pode compartilhar conhecimento:*</span>
+                <span class="destaque">Especifique a área de experiência e temas que você pode compartilhar conhecimento: *</span>
             </div>
              <taxonomy-checkboxes taxonomy="area_atuacao" entity="agent" terms="termos.area_atuacao"></taxonomy-checkboxes>
         </div>
         <div class="row">
-            <h4>Articulação</h4>
+        
             <div class="colunm-full">
                 <span class="destaque">Participa de algum movimento político-cultural? *</span>
             </div>
@@ -55,7 +55,7 @@
                 <input type="radio" name="politicocultural" >   Não
             </label>
             <label class="colunm2">
-                <input type="radio" name="politicocultural" >  Sim*
+                <input type="radio" name="politicocultural" >  Sim
                 <!-- textarea></textarea -->
             </label>
             <div class="colunm-full">
@@ -65,14 +65,14 @@
                 <input type="radio" name="forumcultural" >  Não
             </label>
             <label class="colunm2">
-                <input type="radio" name="forumcultural" > Sim*
+                <input type="radio" name="forumcultural" > Sim
                 <!-- textarea></textarea -->
             </label>
             <div class="colunm-full">
                 <span class="destaque">Participa de instância de representação junto ao Ministério da Cultura? *</span>
             </div>
             <label class="colunm1">
-                <input type="checkbox" name="" >  Colegiados*
+                <input type="checkbox" name="" >  Colegiados
                 <!-- textarea></textarea -->
             </label>
             <label class="colunm2">
@@ -88,15 +88,15 @@
                 <!-- textarea></textarea -->
             </label>
             <label class="colunm2">
-                <input type="checkbox" name="" >  Grupo de Trabalho*
+                <input type="checkbox" name="" >  Grupo de Trabalho
                 <!-- textarea></textarea -->
             </label>
             <label class="colunm3">
-                <input type="checkbox" name="" >  Conselhos*
+                <input type="checkbox" name="" >  Conselhos
                 <!-- textarea></textarea -->
             </label>
             <label class="colunm1">
-                <input type="checkbox" name="" >  Outros*
+                <input type="checkbox" name="" >  Outros
                 <!-- textarea></textarea -->
             </label>
             <div class="colunm-full">
@@ -106,7 +106,7 @@
                 <input type="radio" name="poderpublico" > Não
             </label>
             <label class="colunm2">
-                <input type="radio" name="poderpublico" >  Sim*
+                <input type="radio" name="poderpublico" >  Sim
                 <!-- textarea></textarea -->
             </label>
 
