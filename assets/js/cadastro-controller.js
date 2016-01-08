a82468866067d714ae21a1ebfabf336f15a6320c
Gabriel Avaloni <avaloni@users.noreply.github.com>
2016-01-08 18:01:28 -0200

    incluindo taxonomias e os metadados específicos de cada tipo de agente no controller da tela de detalhamento

--- assets/js/cadastro-controller.js
+++ assets/js/cadastro-controller.js
@@ -967,6 +967,7 @@
 
     app.controller('DetailCtrl',['$scope', 'Entity', '$http', '$timeout', '$location', function($scope, Entity, $http, $timeout, $location){
         extendController($scope, $timeout);
+        $scope.termos = termos;
         $http.get(MapasCulturais.createUrl('admin','user') + '?id='+$location.search()['id'])
             .success(function(data){
                 var responsavel = {
@@ -980,13 +981,41 @@
                 };
                 var entidade = {
                     'id': data.agenteEntidade,
+                    '@select':  'id,rcv_tipo,name,nomeCompleto,cnpj,representanteLegal,' +
+                                'tipoPontoCulturaDesejado,tipoOrganizacao,' +
+                                'emailPrivado,telefone1,telefone1_operadora,telefone2,telefone2_operadora,' +
+                                'responsavel_nome,responsavel_email,responsavel_cargo,responsavel_telefone,' +
+                                'geoEstado,geoMunicipio,pais,En_Bairro,En_Num,En_Nome_Logradouro,En_Complemento,' +
+                                'tipoCertificacao,foiFomentado,tipoFomento,tipoFomentoOutros,tipoReconhecimento,edital_num,' +
+                                'edital_ano,edital_projeto_nome,edital_localRealizacao,edital_projeto_etapa,' +
+                                'edital_proponente,edital_projeto_resumo,edital_prestacaoContas_envio,' +
+                                'edital_prestacaoContas_status,edital_projeto_vigencia_inicio,' +
+                                'edital_projeto_vigencia_fim,outrosFinanciamentos,outrosFinanciamentos_descricao,' +
+                                'rcv_Ds_Edital',
                     '@permissions': 'view'
                 };
                 var ponto = {
                     'id': data.agentePonto,
+                    '@select':  'id,rcv_tipo,longDescription,atividadesEmRealizacaoLink,site,facebook,twitter,googleplus,flickr,diaspora,youtube,instagram,culturadigital,atividadesEmRealizacaoLink,' +
+                                'terms,name,shortDescription,cep,tem_sede,sede_realizaAtividades,mesmoEndereco,pais,geoEstado,geoMunicipio,'+
+                                'En_Bairro,En_Num,En_Nome_Logradouro,En_Complemento,localRealizacao_estado,localRealizacao_cidade,'+
+                                'localRealizacao_cidade,localRealizacao_espaco,location,' +
+		                        'participacaoMovPolitico,participacaoForumCultura,parceriaPoderPublico, simMovimentoPoliticoCultural, simForumCultural, simPoderPublico,' +
+		                        'pontoOutrosRecursosRede,pontoNumPessoasNucleo,pontoNumPessoasColaboradores,' +
+                                'pontoNumPessoasIndiretas,pontoNumPessoasParceiros,pontoNumPessoasApoiadores,pontoNumRedes,' +
+                                'pontoRedesDescricao,pontoMovimentos,pontoEconomiaSolidaria,pontoEconomiaSolidariaDescricao,' +
+                                'pontoEconomiaCultura,pontoEconomiaCulturaDescricao,pontoMoedaSocial,pontoMoedaSocialDescricao,' +
+                                'pontoTrocasServicos,pontoTrocasServicosOutros,pontoContrataServicos,pontoContrataServicosOutros,' +
+                                'pontoInvestimentosColetivos,pontoInvestColetivosOutros,pontoCustoAnual,' +
+		                        'formador1_nome,formador1_email,formador1_telefone,formador1_operadora,formador1_areaAtuacao,' +
+                                'formador1_bio,formador1_facebook,formador1_twitter,formador1_google,espacoAprendizagem1_atuacao,espacoAprendizagem1_tipo,' +
+                                'espacoAprendizagem1_desc,metodologia1_nome,metodologia1_desc,metodologia1_necessidades,metodologia1_capacidade,' +
+                                'metodologia1_cargaHoraria,metodologia1_certificacao',
                     '@permissions': 'view'
                 };
                 $scope.responsavel = Entity.get(responsavel);
+                $scope.entidade = Entity.get(entidade);
+                $scope.ponto = Entity.get(ponto);
             }).error(function(){
                 $scope.messages.show('erro', "O usuário não foi encontrado");
             });
