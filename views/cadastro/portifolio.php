9e19e51517f03a231eb321749f398cdc26ec2424
minc <guiadler@hotmail.com>
2016-03-22 09:44:42 -0300

    Adicionado cada campo de upload no seu próprio controlador para resolver duplicação da barra de progresso

--- views/cadastro/portifolio.php
+++ views/cadastro/portifolio.php
@@ -7,6 +7,9 @@
     $this->cadastroPageClass = 'portfolio page-base-form';
     $this->cadastroLinkContinuar = 'entidadeFinanciamento';
 ?>
+<style>
+
+</style>
 <form name="form_portifolio" ng-controller="PortifolioCtrl">
     <?php $this->part('messages'); ?>
     <div class="form">
@@ -30,13 +33,14 @@
                 </div>
             </div>
 
+
             <label class="colunm-50">
 
                 <p>Caso não possua portfólio online, você também pode anexar arquivos no formato PDF, com no máximo 20MB.</p>
 		<div class="row" ng-controller="PortifolioCtrl">
 			<label class="colunm1">
                 		<span class="destaque">
-				Portfólio Online*
+				                  Portfólio Online*
 				<i class='hltip' title="Caso possua um portfólio online, coloque o link aqui.">?</i></span>
 	        		<input type="text" name="atividadesEmRealizacaoLink" placeholder="http://" ng-blur="save_field('atividadesEmRealizacaoLink')" ng-model="agent.atividadesEmRealizacaoLink" />
 			</label>
@@ -47,29 +51,30 @@
                     <a href="http://docs.cultura.gov.br/products/files/doceditor.aspx?fileid=138&doc=NEQxOFBKRmNORzhYaVJ1NGNZUC8xNG1EMC9WaWgvRkFqbGc0MlhOV3BVZz0_IjEzOCI1" target="_blank">Clique aqui</a> para baixar um modelo com orientações.
                 </p>
             </label>
-              <div ng-if="agent_entidade.tipoOrganizacao == 'coletivo'">
+      </div>
+      <div class="row" ng-controller="ImageUploadCtrl">
+        <div ng-if="agent_entidade.tipoOrganizacao == 'coletivo'">
           <h4>Carta de Autorização de Coletivo sem Constituição Jurídica</h4>
-
-        <div class="colunm-20">
-            <div class="file-item">
-                <a ng-if="agent.files.ata" href="#" class="exclui" ng-click="deleteFile(agent.files.ata)" title="Excluir Portfólio">x</a>
-                <div type="file" ngf-select="uploadFile($file, 'ata')" accept="config.pdf.validation" ngf-max-size="config.pdf.maxUploadSize" title="{{agent.files.ata ? 'Clique para alterar a Carta' : 'Clique para incluir a Carta'}}">
+            <div class="colunm-20">
+              <div class="file-item">
+                  <a ng-if="agent.files.ata" href="#" class="exclui" ng-click="deleteFile(agent.files.ata)" title="Excluir Portfólio">x</a>
+                  <div type="file" ngf-select="uploadFile($file, 'ata')" accept="config.pdf.validation" ngf-max-size="config.pdf.maxUploadSize" title="{{agent.files.ata ? 'Clique para alterar a Carta' : 'Clique para incluir a Carta'}}">
                     <img ng-if="!agent.files.ata" src="<?php $this->asset('img/incluir_img.png') ?>" width="160" height="138">
                     <img ng-if="agent.files.ata" src="<?php $this->asset('img/pdflogo.png') ?>" width="160" height="138">
-                </div>
+                  </div>
             </div>
             <a ng-if="agent.files.ata" href="{{agent.files.ata.url}}" target="_blank">Baixar Arquivo</a>
             <div class="progress row" ng-show="f.progress >= 0">
                 <span style="width:{{f.progress}}%;" ng-bind="f.progress + '%'"></span>
             </div>
-        </div>
+          </div>
               <label class="colunm-50">
                   Precisa de ajuda para montar sua carta?
                   <br>
                   <a href="<?php $this->asset('pdf/Carta_de_Apoio_da_Comunidade_à_Coletivo vs 3.pdf') ?>" target="_blank">Clique aqui</a> para baixar um modelo com orientações.
                 </label>
-              </div>
-            </div>
+        </div>
+      </div>
 
         <div class="row">
             <h4>Cartas de Reconhecimento</h4>
@@ -91,7 +96,7 @@
                   <span style="width:{{f.progress}}%;" ng-bind="f.progress + '%'"></span>
               </div>
             </div>
-            <div class="colunm-20">
+            <div class="colunm-20" >
               <div class="file-item">
                   <a ng-if="agent.files.carta2" href="#" class="exclui" ng-click="deleteFile(agent.files.carta2)" title="Excluir Portfólio">x</a>
                   <div type="file" ngf-select="uploadFile($file, 'carta2')" accept="config.pdf.validation" ngf-max-size="config.pdf.maxUploadSize" title="{{agent.files.carta2 ? 'Clique para alterar a carta de recomendação' : 'Clique para incluir uma carta de recomendação'}}">
